2d2ab8ee994c0e08b903742794cb9a68b0c539b9
aeemblt <tattapan.w@blttech.net>
2025-06-24 15:08:13 +0700

    Add Dockerfile for multi-stage build and update next.config.ts for standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  // Enable if you're using experimental features
+  // experimental: {
+  //   outputFileTracingRoot: undefined, // defaults to process.cwd()
+  // },
 };
 
 export default nextConfig;
